fbe3f8d3607b52ac971a6db4f87b9c4e3218f661
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2026-05-29 15:10:05 +0300

    Place object

--- app/src/main/java/com/example/spotter/model/Place.kt
+++ app/src/main/java/com/example/spotter/model/Place.kt
@@ -1,3 +1,16 @@
-package com.example.spotter.model
+package com.example.mapdiary.model
 
-data class Place()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "places_table")
+data class Place(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val description: String,
+    val latitude: Double? = null,
+    val longitude: Double? = null,
+    val imagePath: String? = null,
+    val tags: List<String> = emptyList()
+)
