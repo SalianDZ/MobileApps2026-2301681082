5373c4d6df22411f5d0bb633c0da46eb0cb7cdd8
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2026-05-29 15:16:07 +0300

    PlaceDao

--- app/src/main/java/com/example/spotter/data/Converters.kt
+++ app/src/main/java/com/example/spotter/data/Converters.kt
@@ -1,16 +1,14 @@
-package com.example.spotter.data // Внимавай за твоя пакет!
+package com.example.mapdiary.data
 
 import androidx.room.TypeConverter
 
 class Converters {
 
-    // Превръща списъка ["планина", "лято"] в текст "планина,лято"
     @TypeConverter
     fun fromTagsList(tags: List<String>): String {
         return tags.joinToString(separator = ",")
     }
 
-    // Превръща текста "планина,лято" обратно в списък ["планина", "лято"]
     @TypeConverter
     fun toTagsList(tagsString: String): List<String> {
         if (tagsString.isEmpty()) return emptyList()
--- app/src/main/java/com/example/spotter/data/PlaceDao.kt
+++ app/src/main/java/com/example/spotter/data/PlaceDao.kt
@@ -1,4 +1,26 @@
-package com.example.spotter.data
+package com.example.mapdiary.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.mapdiary.model.Place
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface PlaceDao {
+
+    @Query("SELECT * FROM places_table ORDER BY id DESC")
+    fun getAllPlaces(): Flow<List<Place>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPlace(place: Place)
+
+    @Update
+    suspend fun updatePlace(place: Place)
+
+    @Delete
+    suspend fun deletePlace(place: Place)
 }
